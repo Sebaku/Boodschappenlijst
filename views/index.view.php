5cefb2d5a659510de677dd273443607a40da8600
Sebaku <SebastiaanVliet@gmail.com>
2024-03-19 15:48:23 +0100

    Added header and updated Nav

--- views/index.view.php
+++ views/index.view.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Boodschappenlijst</title>
-</head>
-<body>
-    <nav>
-        <?php require('partials/nav.php');?>
-    </nav>
+<?php
+$title = "Home"; 
+require('partials/header.php');?>
     <table id="table1">
         <tr>
             <th>Product</th>
